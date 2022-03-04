2788834abbefbaec3cf5ecadc9d55d7e33b33652
Ishan Goel <igoel.mail@gmail.com>
2022-03-03 19:43:40 -0800

    Initial GitHub support + detect records which already exist

--- go.mod
+++ go.mod
@@ -3,11 +3,19 @@ module doge-cla
 go 1.17
 
 require (
-	github.com/bwmarrin/discordgo v0.23.2
-	github.com/mehanizm/airtable v0.2.5
+	github.com/bradleyfalzon/ghinstallation v1.1.1
+	github.com/bwmarrin/discordgo v0.24.0
+	github.com/google/go-github/v43 v43.0.0
+	github.com/mehanizm/airtable v0.2.6
+	gopkg.in/go-playground/webhooks.v5 v5.17.0
 )
 
 require (
-	github.com/gorilla/websocket v1.4.0 // indirect
-	golang.org/x/crypto v0.0.0-20181030102418-4d3f4d9ffa16 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/google/go-github/v29 v29.0.3 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 )
--- main.go
+++ main.go
@@ -1,37 +1,57 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
+	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
+	"github.com/bradleyfalzon/ghinstallation"
 	"github.com/bwmarrin/discordgo"
+	v3 "github.com/google/go-github/v43/github"
 	"github.com/mehanizm/airtable"
+	ghwebhooks "gopkg.in/go-playground/webhooks.v5/github"
 )
 
 var (
-	dg    *discordgo.Session
-	shibe *discordgo.Role
-	table *airtable.Table
+	dg        *discordgo.Session
+	shibeRole *discordgo.Role
+	table     *airtable.Table
 
 	c = getCfg()
 
 	msgIDToUser = make(map[string]*discordgo.User, 100)
+	ghUser      = make(map[string]*v3.User, 100)
+
+	//appID = int64(142994) // your app id goes here
+	//user  = "quackduck"
+	//
+	//privkey = ""
+
+	installationID int64
+	itr            *ghinstallation.Transport
+	ghBotSlug      = ""
 )
 
 type config struct {
-	Token          string
-	TargetRoleName string
-	ServerID       string
-	ChannelID      string
-	AirtableKey    string
-	AirtableBaseID string
+	Token             string
+	TargetRoleName    string
+	ServerID          string
+	ChannelID         string
+	AirtableKey       string
+	AirtableBaseID    string
+	AirtableTableName string
+	GHPrivKey         string
+	GHUser            string
+	GHAppID           int64
 }
 
 func getCfg() *config {
@@ -73,15 +93,43 @@ func main() {
 		return
 	}
 
-	fmt.Println("Bot is running! Press CTRL-C to exit.")
+	atr, err := ghinstallation.NewAppsTransport(http.DefaultTransport, c.GHAppID, []byte(c.GHPrivKey))
+	if err != nil {
+		fmt.Println("error creating GitHub app client")
+		return
+	}
+
+	//
+	//installation, _, err := v3.NewClient(&http.Client{Transport: atr}).Apps.FindOrganizationInstallation(context.TODO(), orgID)
+	//if err != nil {
+	//	log.Fatalf("error finding organization installation: %v", err)
+	//}
+
+	installation, _, err := v3.NewClient(&http.Client{Transport: atr}).Apps.FindUserInstallation(context.TODO(), c.GHUser)
+	if err != nil {
+		fmt.Printf("error finding installation: %v", err)
+		return
+	}
+
+	ghBotSlug = *installation.AppSlug
+	installationID = installation.GetID()
+	itr = ghinstallation.NewFromAppsTransport(atr, installationID)
+
+	fmt.Printf("Successfully initialized GitHub app client, installation-id:%d expected-events:%v\n", installationID, installation.Events)
+	http.HandleFunc("/github", Handle)
+	err = http.ListenAndServe("0.0.0.0:3000", nil)
+	if err != nil && err != http.ErrServerClosed {
+		fmt.Println(err)
+		return
+	}
+
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
-
 	dg.Close()
 }
 
-// finds the shibe role
+// finds the shibeRole role
 func setup() error {
 	roles, err := dg.GuildRoles(c.ServerID)
 	if err != nil {
@@ -90,16 +138,17 @@ func setup() error {
 	fmt.Println("Looking through server roles...")
 	for i := range roles {
 		if roles[i].Name == c.TargetRoleName {
-			shibe = roles[i]
+			shibeRole = roles[i]
 		}
 	}
-	if shibe == nil {
+	if shibeRole == nil {
 		return errors.New("no '" + c.TargetRoleName + "' role detected, you should add one")
 	}
 	fmt.Println("Found the role!")
 
 	client := airtable.NewClient(c.AirtableKey)
-	table = client.GetTable(c.AirtableBaseID, "Table 1")
+	//client.SetBaseURL("https://api.airtable.com/v0/")
+	table = client.GetTable(c.AirtableBaseID, c.AirtableTableName)
 	return nil
 }
 
@@ -129,13 +178,14 @@ func pickRandom(a []string) string {
 
 func msgReact(s *discordgo.Session, m *discordgo.MessageReactionAdd) {
 	if user, ok := msgIDToUser[m.MessageID]; ok {
+		delete(msgIDToUser, m.MessageID)
 		if m.UserID == user.ID && (m.Emoji.Name == "👍") {
-			addApprovedUser(user, messageToLink(m))
+			addApprovedDiscordUser(user, discordMessageToLink(m))
 			_, err := s.ChannelMessageSend(c.ChannelID, "Wow, thanks for your acceptance, "+user.Mention()+". I'm adding the Shibe role yahoo!")
 			if err != nil {
 				fmt.Println("Couldn't send a message. error: ", err)
 			}
-			err = s.GuildMemberRoleAdd(c.ServerID, user.ID, shibe.ID)
+			err = s.GuildMemberRoleAdd(c.ServerID, user.ID, shibeRole.ID)
 			if err != nil {
 				_, err = s.ChannelMessageSend(c.ChannelID, "OH NOOOOO I got an error while trying to make you a shibe: "+err.Error()+"\n Please send much stern messages to Ishan#9106")
 				if err != nil {
@@ -146,15 +196,19 @@ func msgReact(s *discordgo.Session, m *discordgo.MessageReactionAdd) {
 	}
 }
 
-func messageToLink(m *discordgo.MessageReactionAdd) string {
+func discordMessageToLink(m *discordgo.MessageReactionAdd) string {
 	return fmt.Sprintf("https://discord.com/channels/%s/%s/%s", m.GuildID, m.ChannelID, m.MessageID)
 }
 
-func addApprovedUser(user *discordgo.User, messageLink string) {
-	addToAirtable(user.String(), "Discord: Agreed to the CLA by reacting with 👍 here: "+messageLink) // TODO: include some kind of message ID/link.
+func addApprovedDiscordUser(user *discordgo.User, messageLink string) {
+	addToAirtable(user.String(), "Discord: Agreed to the CLA by reacting with 👍 here: "+messageLink)
 }
 
 func addToAirtable(name string, notes string) {
+	exists, _ := doesRecordExist(name)
+	if exists {
+		return
+	}
 	recordsToSend := &airtable.Records{
 		Records: []*airtable.Record{
 			{
@@ -176,3 +230,133 @@ func addToAirtable(name string, notes string) {
 	}
 	fmt.Println("Sent these records to Airtable:", string(j))
 }
+
+func doesRecordExist(name string) (bool, error) {
+	checkExistsURLValues := url.Values{}
+	checkExistsURLValues.Add("fields", "Name")
+	checkExistsURLValues.Add("filterByFormula", fmt.Sprintf("{Name = %s}", name))
+	records, err := table.GetRecordsWithParams(checkExistsURLValues)
+	if err != nil {
+		return false, err
+	}
+	if len(records.Records) > 0 { // there are duplicates
+		return true, nil
+	}
+	return false, nil
+}
+
+func Handle(response http.ResponseWriter, request *http.Request) {
+	hook, err := ghwebhooks.New()
+	if err != nil {
+		return
+	}
+
+	payload, err := hook.Parse(request, []ghwebhooks.Event{ghwebhooks.ReleaseEvent, ghwebhooks.PullRequestEvent, ghwebhooks.IssueCommentEvent, ghwebhooks.PullRequestReviewCommentEvent}...)
+	if err != nil {
+		if err == ghwebhooks.ErrEventNotFound {
+			fmt.Printf("received unregistered GitHub event: %v\n", err)
+			response.WriteHeader(http.StatusOK)
+		} else {
+			fmt.Printf("received malformed GitHub event: %v\n", err)
+			response.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	switch payload := payload.(type) {
+	case ghwebhooks.PullRequestPayload:
+		fmt.Println("received pull request event")
+		if payload.Action != "created" {
+			go PREvent(&payload)
+		}
+	case ghwebhooks.IssueCommentPayload:
+		fmt.Println("received pull request comment")
+		go PRCommentEvent(&payload)
+	default:
+		fmt.Println("missing handler for event", payload)
+	}
+	response.WriteHeader(http.StatusOK)
+}
+
+func GetV3Client() *v3.Client {
+	return v3.NewClient(&http.Client{Transport: itr})
+}
+
+func PREvent(p *ghwebhooks.PullRequestPayload) { // TODO: check if user already registered
+	fmt.Println(ToJSON(p))
+	if exists, _ := doesRecordExist(p.Sender.Login); exists {
+		return
+	}
+	msg := "Hello @" + p.Sender.Login + "!\n\n" +
+		"Thanks for your pull request! Please read the Dogecoin CLA & COC here: https://example.com/TODO\n\n" +
+		"Reply with 👍 to accept it."
+	t := time.Now()
+	P, _, err := GetV3Client().Issues.CreateComment(
+		context.TODO(),
+		p.Repository.Owner.Login,
+		p.Repository.Name,
+		int(p.Number),
+		&v3.IssueComment{
+			NodeID:    &p.Repository.NodeID,
+			Body:      &msg,
+			CreatedAt: &t,
+			UpdatedAt: &t,
+		},
+	)
+	if err != nil {
+		fmt.Printf("making comment error: %v\n", err)
+	}
+	fmt.Println(ToJSON(P))
+}
+
+func PRCommentEvent(p *ghwebhooks.IssueCommentPayload) {
+	if p.Sender.Login == ghBotSlug+"[bot]" { // don't respond to self
+		return
+	}
+	fmt.Println(ToJSON(p))
+	if p.Comment.Body == "👍" && p.Sender.Login == p.Issue.User.Login {
+		addApprovedGHUser(p.Sender.Login, p.Comment.URL)
+		msg := "You have now accepted the CLA! @" + p.Sender.Login + " is now a shibe!"
+		t := time.Now()
+		reply, _, err := GetV3Client().Issues.CreateComment( // pr replies are sent through the issues api for some reason
+			context.TODO(),
+			p.Repository.Owner.Login,
+			p.Repository.Name,
+			int(p.Issue.Number),
+			&v3.IssueComment{
+				NodeID:    &p.Repository.NodeID,
+				Body:      &msg,
+				CreatedAt: &t,
+				UpdatedAt: &t,
+			},
+		)
+		if err != nil {
+			fmt.Printf("making comment returned error: %v\n", err)
+		}
+		fmt.Println(ToJSON(reply))
+	}
+}
+
+// ToJSON is a convenience method for serializing to JSON.
+func ToJSON(v interface{}) string {
+	s, _ := json.MarshalIndent(v, "", "   ")
+	return string(s)
+}
+
+func addApprovedGHUser(user string, messageLink string) {
+	addToAirtable(user, "GitHub: Agreed to the CLA by messaging 👍 here: "+messageLink)
+}
+
+//for _, v := range records.Records {
+//	var nameDup, notesDup interface{}
+//	var ok bool
+//	if nameDup, ok = v.Fields["Name"]; !ok {
+//		continue
+//	}
+//	if notesDup, ok = v.Fields["Notes"]; ok {
+//		continue
+//	}
+//	if nameDup == name && notesDup == notes { // already exists
+//		return true, nil
+//	}
+//}
